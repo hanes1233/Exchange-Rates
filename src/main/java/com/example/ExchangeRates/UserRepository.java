4b484837793245e2c38297843846974c547541e6
hanes1233 <pa7sha@seznam.cz>
2025-02-02 21:24:03 +0100

    Updates 2025

--- src/main/java/com/example/ExchangeRates/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ExchangeRates;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<Rates, Long> {
-    Optional<Rates> findByCountry(String name);
-}
